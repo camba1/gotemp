27c122da79950583c3e876de9191c03061bafdb1
Juan Peredo <jperedo@bolbeck.com>
2020-05-22 15:58:34 -0500

    initial commit

---
+++ promotion/server/statements/doc.go
@@ -0,0 +1,9 @@
+/*
+Statements package contains service specific error messages and sql statements that will be encountered while running the
+promotion service. It is composed of two files:
+	- errorstatements.go contains all the error messages. The error messages support multiple languages. Language used
+	  depends on the value of the 'language' variable. If not explicitly set, it defaults to english.
+	- sqlstatements.go contains all the sql statements to be run against the DB
+*/
+
+package statements
