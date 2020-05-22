d5b71a98d09cce203bc7aab0db9279500941ac99
Juan Peredo <jperedo@bolbeck.com>
2020-05-22 10:20:26 -0500

    converted two constants to private since we have public methods for them

--- globalerrors/globalerrors.go
+++ globalerrors/globalerrors.go
@@ -7,8 +7,8 @@ type SrvError string
 const (
 	srvNoStartTxt           SrvError = "Unable to start %s server. Error: %v \n"
 	srvNoHandlerTxt         SrvError = "Unable to register service handler. Error: %v"
-	DbNoConnectionTxt       SrvError = "Unable to connect to DB %s. Error: %v\n"
-	DbNoConnectionStringTxt SrvError = "Unable to find DB connection string. Please set environment variable %s \n"
+	dbNoConnectionTxt       SrvError = "Unable to connect to DB %s. Error: %v\n"
+	dbNoConnectionStringTxt SrvError = "Unable to find DB connection string. Please set environment variable %s \n"
 )
 
 func (ge *SrvError) SrvNoStart(serviceName string, err error) string {
@@ -16,11 +16,11 @@ func (ge *SrvError) SrvNoStart(serviceName string, err error) string {
 }
 
 func (ge *SrvError) DbNoConnection(dbName string, err error) string {
-	return fmt.Sprintf(string(DbNoConnectionTxt), dbName, err)
+	return fmt.Sprintf(string(dbNoConnectionTxt), dbName, err)
 }
 
 func (ge *SrvError) DbNoConnectionString(envVarName string) string {
-	return fmt.Sprintf(string(DbNoConnectionStringTxt), envVarName)
+	return fmt.Sprintf(string(dbNoConnectionStringTxt), envVarName)
 }
 
 func (ge *SrvError) SrvNoHandler(err error) string {
