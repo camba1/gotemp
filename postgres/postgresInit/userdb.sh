cd38005fdedcdd310a5fc5afcbb73b6d2b25c6a0
Juan Peredo <jperedo@bolbeck.com>
2020-05-27 15:18:58 -0500

    initial commit

---
+++ postgres/postgresInit/userdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+POSTGRES="psql --username postgres"
+
+echo "Creating database: appuser"
+
+$POSTGRES <<EOSQL
+CREATE DATABASE appuser;
+EOSQL
+
+echo "Creating schema..."
+psql -d appuser -a -U postgres -f ./docker-entrypoint-initdb.d/userSchema_sql.txt
