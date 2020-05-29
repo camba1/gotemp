9ce3b4e45ef76f1eb21068069c94c1677264323e
Juan Peredo <jperedo@bolbeck.com>
2020-05-29 17:46:37 -0500

    Added user email and company to appuser table

--- user/server/statements/sqlstatements.go
+++ user/server/statements/sqlstatements.go
@@ -4,9 +4,9 @@ type usrSql string
 
 // Create update delete statements
 const (
-	SqlInsert usrSql = `insert into appuser (firstname , lastname, validfrom, validthru, active, pwd) 
-						VALUES ($1, $2, $3, $4, $5, $6)
-						RETURNING id, firstname , lastname, validfrom, validthru, active, pwd, name`
+	SqlInsert usrSql = `insert into appuser (firstname , lastname, validfrom, validthru, active, pwd, email, company) 
+						VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+						RETURNING id, firstname , lastname, validfrom, validthru, active, pwd, name, email, company`
 
 	SqlUpdate usrSql = ` update appuser set 
 						 firstname = $1,
@@ -14,9 +14,11 @@ const (
 						 validfrom = $3,
 						 validthru = $4,
 						 active = $5,
-						 pwd = $6
-						where id = $7
-						RETURNING id, firstname , lastname, validfrom, validthru, active, pwd, name`
+						 pwd = $6,
+						 email = %7,
+						 company = $8
+						where id = $9
+						RETURNING id, firstname , lastname, validfrom, validthru, active, pwd, name, email, company`
 
 	SqlDelete usrSql = "delete from appuser where id = $1"
 )
@@ -24,12 +26,12 @@ const (
 // Select statements
 const (
 	SqlSelectById usrSql = `select 
-								id, firstname , lastname, validfrom, validthru, active, pwd, name 
+								id, firstname , lastname, validfrom, validthru, active, pwd, name, email, company 
 							from appuser 
 							where id = $1`
 
 	SqlSelectAll usrSql = ` select 
-								id, firstname , lastname, validfrom, validthru, active, pwd, name 
+								id, firstname , lastname, validfrom, validthru, active, pwd, name, email, company 
 							from appuser`
 )
 
