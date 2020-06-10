f46ff6bde153708469eaf4af6a24be4505ab4655
Juan Peredo <jperedo@bolbeck.com>
2020-06-10 09:32:17 -0500

    New auditmsgheader to be handle the header information in one place

--- globalUtils/audit.go
+++ globalUtils/audit.go
@@ -1,16 +1,24 @@
 package globalUtils
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
 
-const topic = "Audit"
+const (
+	AuditTopic       = "Audit"
+	AuditQueueInsert = "Audit.Insert"
+)
 
 type auditMsg struct {
 	topic        string
 	objectToSend []byte
-	header       auditMsgHeader
+	header       AuditMsgHeader
 }
 
-func (a auditMsg) Header() auditMsgHeader {
+func (a auditMsg) Header() AuditMsgHeader {
 	return a.header
 }
 
@@ -22,9 +30,47 @@ func (a auditMsg) Topic() string {
 	return a.topic
 }
 
-type auditMsgHeader map[string]string
+type AuditMsgHeader map[string]string
 
-func NewAuditMsg(serviceName, actionFunc, actionType string, performedBy, objectId int64, objectToSend []byte) (*auditMsg, error) {
+type AuditMsgHeaderStruct struct {
+	ServiceName string
+	ActionFunc  string
+	ActionType  string
+	ObjectId    int64
+	PerformedBy int64
+	ActionTime  time.Time
+	ObjectName  string
+}
+
+func (a *AuditMsgHeaderStruct) GetObjectName() string {
+	return a.ObjectName
+}
+
+func (a *AuditMsgHeaderStruct) GetActionTime() time.Time {
+	return a.ActionTime
+}
+
+func (a *AuditMsgHeaderStruct) GetPerformedBy() int64 {
+	return a.PerformedBy
+}
+
+func (a *AuditMsgHeaderStruct) GetObjectId() int64 {
+	return a.ObjectId
+}
+
+func (a *AuditMsgHeaderStruct) GetActionType() string {
+	return a.ActionType
+}
+
+func (a *AuditMsgHeaderStruct) GetActionFunc() string {
+	return a.ActionFunc
+}
+
+func (a *AuditMsgHeaderStruct) GetServiceName() string {
+	return a.ServiceName
+}
+
+func NewAuditMsg(serviceName, actionFunc, actionType string, performedBy int64, objectName string, objectId int64, objectToSend []byte) (*auditMsg, error) {
 	var missingFields string
 	if serviceName == "" {
 		missingFields += " serviceName,"
@@ -49,15 +95,47 @@ func NewAuditMsg(serviceName, actionFunc, actionType string, performedBy, object
 			missingFields[1:len(missingFields)-1])
 	}
 	aud := auditMsg{
-		topic:        topic,
+		topic:        AuditTopic,
 		objectToSend: objectToSend,
-		header: auditMsgHeader{
+		header: AuditMsgHeader{
 			"service":     serviceName,
 			"actionFunc":  actionFunc,
 			"actionType":  actionType,
-			"objectId":    string(objectId),
-			"performedBy": string(performedBy),
+			"objectId":    strconv.FormatInt(objectId, 10),
+			"performedBy": strconv.FormatInt(performedBy, 10),
+			"actionTime":  time.Now().Format(DateLayoutISO),
+			"objectName":  objectName,
 		},
 	}
+	//log.Printf(" objid : %v, objidstr: %s ",  objectId, strconv.FormatInt(performedBy, 10))
 	return &aud, nil
+}
+
+func AuditMsgHeaderToStruct(header AuditMsgHeader) (*AuditMsgHeaderStruct, error) {
+	if header == nil {
+		return nil, fmt.Errorf("message header cannot be nil when trying to convert to struct")
+	}
+	objectId, err := strconv.ParseInt(header["objectId"], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	performedBy, err := strconv.ParseInt(header["performedBy"], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	actionTime, err := time.Parse(DateLayoutISO, header["actionTime"])
+	if err != nil {
+		log.Printf("Unable to Format date %v\n", header["actionTime"])
+	}
+	headerStruct := &AuditMsgHeaderStruct{
+		ServiceName: header["service"],
+		ActionFunc:  header["actionFunc"],
+		ActionType:  header["actionType"],
+		ObjectId:    objectId,
+		PerformedBy: performedBy,
+		ActionTime:  actionTime,
+		ObjectName:  header["objectName"],
+	}
+
+	return headerStruct, nil
 }
