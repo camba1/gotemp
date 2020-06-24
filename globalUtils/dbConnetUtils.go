17d27f5809e021a0c30f8dfee9008e8ad09db1eb
Juan Peredo <jperedo@bolbeck.com>
2020-06-24 16:37:16 -0500

    section to connect to arangoDB

--- globalUtils/dbConnetUtils.go
+++ globalUtils/dbConnetUtils.go
@@ -2,6 +2,8 @@ package globalUtils
 
 import (
 	"context"
+	adb "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
 	"github.com/jackc/pgx/v4"
 	"log"
 	"time"
@@ -41,4 +43,67 @@ func (p *PgxDBConnect) ConnectToDBWithRetry(databaseName string, connectionStrin
 		}
 	}
 	return dbConn, nil
+}
+
+//DbConnParams: Generic connection parameters to connect to the DB
+type DbConnParams struct {
+	Address  string
+	Username string
+	Password string
+}
+
+//ArangoConnect: Handles connections to ArangoDB
+type ArangoConnect struct{}
+
+//connectToDB: Try to connect to the DB. Return true if connection was successful, false otherwise
+func (ac *ArangoConnect) ConnectToDB(databaseName string, connParams *DbConnParams) (adb.Database, bool, error) {
+
+	// Create an HTTP connection to the database
+	conn, err := http.NewConnection(http.ConnectionConfig{
+		Endpoints: []string{"http://" + connParams.Address},
+	})
+	if err != nil {
+		return nil, false, nil
+	}
+	// Create a client
+	cli, err := adb.NewClient(adb.ClientConfig{
+		Connection: conn,
+		//Authentication: adb.JWTAuthentication('ere','ere'),
+		Authentication: adb.BasicAuthentication(connParams.Username, connParams.Password),
+	})
+	if err != nil {
+		return nil, false, err
+	}
+
+	ctx := context.Background()
+	db, err := cli.Database(ctx, databaseName)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return db, true, nil
+}
+
+//connectToDBWithRetry: Attempts to connect to the DB every 3s for up to maxRetries in case of connection failure
+func (ac *ArangoConnect) ConnectToDBWithRetry(databaseName string, connParams *DbConnParams) (adb.Database, error) {
+	maxRetries := 5
+	var db adb.Database
+	var connected bool
+	var err error
+	for i := 1; i <= maxRetries; i++ {
+		db, connected, err = ac.ConnectToDB(databaseName, connParams)
+		if !connected {
+			if i >= maxRetries {
+				log.Printf(glErr.DbNoConnection(databaseName, err))
+				return nil, err
+			} else {
+				log.Printf(glErr.DbConnectRetry(i, err))
+				time.Sleep(3 * time.Second)
+			}
+		} else {
+			break
+		}
+	}
+	log.Println("Connected to DB")
+	return db, nil
 }
