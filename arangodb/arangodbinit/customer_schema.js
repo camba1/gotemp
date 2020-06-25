f8be0a4e2001c85e527e81dac53e7a2aebb5b102
Juan Peredo <jperedo@bolbeck.com>
2020-06-25 14:31:14 -0500

    changed fields to camel case. Added extra field to one of the customer records

--- arangodb/arangodbinit/customer_schema.js
+++ arangodb/arangodbinit/customer_schema.js
@@ -3,11 +3,11 @@ db._useDatabase('customer');
 db._create('customer');
 db._createEdgeCollection("isparentof");
 
-db.customer.insert({"_key": "ducksrus", "name": "Ducks R Us", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
-db.customer.insert({"_key": "ducksrusd", "name": "Ducks R Us Dock", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
-db.customer.insert({"_key": "ducksrusb", "name": "Ducks R Us Bill", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
-db.customer.insert({"_key": "patoloco", "name": "Pato Loco Inc", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
-db.customer.insert({"_key": "canard", "name": "Canard Oui Oui", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
+db.customer.insert({"_key": "ducksrus", "name": "Ducks R Us", "validFrom": "2019-01-02", "validThru": "2021-01-02", "externalId":"12345"});
+db.customer.insert({"_key": "ducksrusd", "name": "Ducks R Us Dock", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+db.customer.insert({"_key": "ducksrusb", "name": "Ducks R Us Bill", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+db.customer.insert({"_key": "patoloco", "name": "Pato Loco Inc", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+db.customer.insert({"_key": "canard", "name": "Canard Oui Oui", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
 
-db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusd", "Type": "soldto", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
-db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusb", "Type": "payto", "validfrom": "2019-01-02", "validthru": "2021-01-02"});
+db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusd", "Type": "soldto", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusb", "Type": "payto", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
