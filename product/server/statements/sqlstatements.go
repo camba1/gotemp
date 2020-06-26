85e062c5223983cb6bcd99968aae986237687588
Juan Peredo <jperedo@bolbeck.com>
2020-06-26 17:42:26 -0500

    removed ols SQL statments and added a AQL query

--- product/server/statements/sqlstatements.go
+++ product/server/statements/sqlstatements.go
@@ -5,39 +5,43 @@ type custSql string
 var MaxRowsToFetch = 200
 
 // Create update delete statements
-const (
-	SqlInsert custSql = `insert into promotion (name, description, validfrom, validthru, customerid, active, approvalstatus, prevapprovalstatus) 
-						VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-						RETURNING id, name, description, validfrom, validthru, customerid, active, approvalstatus,  prevapprovalstatus `
-
-	SqlUpdate custSql = ` update promotion set 
-						 name = $1,
-						 description = $2,
-						 validfrom = $3,
-						 validthru = $4,
-						 customerid = $5,
-						 active = $6,
-						 approvalstatus = $7,
-						 prevapprovalstatus = $8
-						where id = $9
-						RETURNING id, name, description, validfrom, validthru, customerid, active, approvalstatus,  prevapprovalstatus`
-
-	SqlDelete custSql = "delete from promotion where id = $1"
-)
+//const (
+//	SqlInsert custSql = `insert into promotion (name, description, validfrom, validthru, customerid, active, approvalstatus, prevapprovalstatus)
+//						VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+//						RETURNING id, name, description, validfrom, validthru, customerid, active, approvalstatus,  prevapprovalstatus `
+//
+//	SqlUpdate custSql = ` update promotion set
+//						 name = $1,
+//						 description = $2,
+//						 validfrom = $3,
+//						 validthru = $4,
+//						 customerid = $5,
+//						 active = $6,
+//						 approvalstatus = $7,
+//						 prevapprovalstatus = $8
+//						where id = $9
+//						RETURNING id, name, description, validfrom, validthru, customerid, active, approvalstatus,  prevapprovalstatus`
+//
+//	SqlDelete custSql = "delete from promotion where id = $1"
+//)
 
 // Select statements
 const (
-	SqlSelectById custSql = `select 
-								id, name, description, validfrom, validthru, 
-								customerid, active, approvalstatus,  prevapprovalstatus 
-							from promotion 
-							where id = $1`
+	//SqlSelectById custSql = `select
+	//							id, name, description, validfrom, validthru,
+	//							customerid, active, approvalstatus,  prevapprovalstatus
+	//						from promotion
+	//						where id = $1`
 
-	SqlSelectAll custSql = ` select 
-								id, name, description, validfrom, validthru, 
-								customerid, active, approvalstatus,  prevapprovalstatus 
-							from promotion %s
-							FETCH FIRST %d ROWS ONLY`
+	//SqlSelectAll custSql = ` select
+	//							id, name, description, validfrom, validthru,
+	//							customerid, active, approvalstatus,  prevapprovalstatus
+	//						from promotion %s
+	//						FETCH FIRST %d ROWS ONLY`
+
+	SqlSelectAll custSql = `FOR p IN product %s
+								LIMIT %d
+								RETURN p`
 )
 
 func (p custSql) String() string {
