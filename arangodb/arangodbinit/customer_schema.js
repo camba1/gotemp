7832706f02e5dec932c6d6390aa807f14ffae6c3
Juan Peredo <jperedo@bolbeck.com>
2020-06-25 21:25:27 -0500

    changed dates based data based on the way timestamps are stored by default

--- arangodb/arangodbinit/customer_schema.js
+++ arangodb/arangodbinit/customer_schema.js
@@ -3,11 +3,18 @@ db._useDatabase('customer');
 db._create('customer');
 db._createEdgeCollection("isparentof");
 
-db.customer.insert({"_key": "ducksrus", "name": "Ducks R Us", "validFrom": "2019-01-02", "validThru": "2021-01-02", "externalId":"12345"});
-db.customer.insert({"_key": "ducksrusd", "name": "Ducks R Us Dock", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
-db.customer.insert({"_key": "ducksrusb", "name": "Ducks R Us Bill", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
-db.customer.insert({"_key": "patoloco", "name": "Pato Loco Inc", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
-db.customer.insert({"_key": "canard", "name": "Canard Oui Oui", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+
+
+db.customer.insert({"_key": "ducksrus","name": "Ducks R Us", "validityDates": {"validFrom": {"seconds": 1592692598,"nanos": 274583000}, "validThru": {"seconds": 1615792598, "nanos": 274584000}}, "externalId":"12345"});
+db.customer.insert({"_key": "ducksrusd","name": "Ducks R Us Dock", "validityDates": {"validFrom": {"seconds": 1592692598,"nanos": 274583000}, "validThru": {"seconds": 1615792598, "nanos": 274584000}}, "externalId":"12345"});
+db.customer.insert({"_key": "ducksrusb","name": "Ducks R Us Bill", "validityDates": {"validFrom": {"seconds": 1592692598,"nanos": 274583000}, "validThru": {"seconds": 1615792598, "nanos": 274584000}}, "externalId":"12345"});
+db.customer.insert({"_key": "patoloco","name": "Pato Loco Inc", "validityDates": {"validFrom": {"seconds": 1592692598,"nanos": 274583000}, "validThru": {"seconds": 1615792598, "nanos": 274584000}}, "externalId":"12345"});
+db.customer.insert({"_key": "canard","name": "Canard Oui Oui", "validityDates": {"validFrom": {"seconds": 1592692598,"nanos": 274583000}, "validThru": {"seconds": 1615792598, "nanos": 274584000}}, "externalId":"12345"});
+// db.customer.insert({"_key": "ducksrus", "name": "Ducks R Us", "validFrom": "2019-01-02", "validThru": "2021-01-02", "externalId":"12345"});
+// db.customer.insert({"_key": "ducksrusd", "name": "Ducks R Us Dock", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+// db.customer.insert({"_key": "ducksrusb", "name": "Ducks R Us Bill", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+// db.customer.insert({"_key": "patoloco", "name": "Pato Loco Inc", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
+// db.customer.insert({"_key": "canard", "name": "Canard Oui Oui", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
 
 db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusd", "Type": "soldto", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
 db.isparentof.insert({"_from":"customer/ducksrus" , "_to":"customer/ducksrusb", "Type": "payto", "validFrom": "2019-01-02", "validThru": "2021-01-02"});
