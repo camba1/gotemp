8a4d1ec534dec3c5d12026cf87e8daf216899f4d
Juan Peredo <jperedo@bolbeck.com>
2020-06-28 09:57:05 -0500

    initial commit

---
+++ customer/client/doc.go
@@ -0,0 +1,12 @@
+/*
+customerClient: Client service to interact with the customer service. Client performs the following operations in order and returns
+the results to the console:
+- Authenticate customer
+- Create new customer
+- update the customer
+- select the customer by id
+- delete the customer
+- select the customer id (not found since it was deleted)
+- search for customers
+*/
+package main
---
+++ customer/server/doc.go
@@ -0,0 +1,15 @@
+/*
+Customer server: Main package to instantiate the customer service.
+This package is composed of the following files:
+- customerServer: Main entry point of service. It starts the service, reads environment variables,
+	connects to the database, initializes the broker and sets up the authentication wrapper
+- handler:Handles all the requests made to the service from outside clients
+- validation: Handles data validation routines and sends audit messages to the broker for storage
+
+A note about retrieving data from the database: Since we are dealing with a multi-model No-SQL DB, there is a possibility
+that there will be fields in the DB that are not in the protobuf definition used to interact with data. Any such such
+additional field will be pulled into the extraFields proto struct and passed along to the client.
+As of this writing, this is a read only field and any changes made to it will be discarded before saving the record
+
+*/
+package main
