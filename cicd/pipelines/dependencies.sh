a386bb11152e93e02b31b22ee5e9fffa4dd33fe5
Juan Peredo <jperedo@bolbeck.com>
2020-07-03 18:48:37 -0500

    Calls docker-compose install

--- cicd/pipelines/dependencies.sh
+++ cicd/pipelines/dependencies.sh
@@ -3,7 +3,8 @@
 set -eu
 
 # Add python pip and bash
-apk add --no-cache py-pip bash
+#apk add --no-cache py-pip bash
+apk add --no-cache python3 py3-pip3 bash
 
 # Install docker-compose via pip
 pip install --no-cache-dir docker-compose
