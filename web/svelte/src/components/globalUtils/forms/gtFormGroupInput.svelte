8299eec64a7591e750425dfc922f072c9d9176ac
Juan Peredo <jperedo@bolbeck.com>
2020-07-08 19:19:17 -0500

    initial commit

---
+++ web/svelte/src/components/globalUtils/forms/gtFormGroupInput.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { FormGroup, Input, Label  } from "sveltestrap"
+    export let config
+</script>
+
+{#each config as configItem}
+    <FormGroup>
+        <Label for={configItem.name}>{configItem.text}</Label>
+        <Input
+                type={configItem.type}
+                name={configItem.name}
+                id={configItem.id}
+                placeholder={configItem.placeholder} />
+    </FormGroup>
+{/each}
---
+++ web/svelte/src/components/login.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { Card, CardBody, CardHeader, CardTitle, Button, Form, FormGroup, Input, Label } from "sveltestrap";
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    let userName = "";
+    let pwd = "";
+
+    function sayHello() {
+        dispatch('message', {
+            text: 'Hello!'
+        });
+    }
+
+</script>
+
+<Card class="mb-3 w-50 mx-auto">
+    <CardHeader>
+        <CardTitle>Login</CardTitle>
+    </CardHeader>
+    <CardBody>
+        <Form>
+            <FormGroup>
+                <Label for="userEmail">Email:</Label>
+                <Input
+                        type="email"
+                        name="userEmail"
+                        id="userEmail"
+                        placeholder="Type your email" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="userPassword">Password:</Label>
+                <Input
+                        type="password"
+                        name="userPassword"
+                        id="userPassword"
+                        placeholder="Type your password" />
+            </FormGroup>
+            <!--<Button color="primary" type="submit" >Submit</Button>-->
+            <Button type="submit" on:click={sayHello} >Submit</Button>
+        </Form>
+    </CardBody>
+</Card>
