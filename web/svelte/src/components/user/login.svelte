47f1f048d5312705ec9beb2fbac57a42e61f9363
Juan Peredo <jperedo@bolbeck.com>
2020-07-16 12:16:55 -0500

    Added value binding

--- web/svelte/src/components/user/login.svelte
+++ web/svelte/src/components/user/login.svelte
@@ -1,10 +1,14 @@
 <script>
     import { Card, CardBody, CardHeader, CardTitle, Button, Form, FormGroup, Input, Label } from "sveltestrap";
+    import { httpPost } from '../../globalUtils/api'
     import { createEventDispatcher } from 'svelte';
+
     const dispatch = createEventDispatcher();
 
-    let userName = "";
-    let pwd = "";
+
+    let email; // = "duck@mymail.com";
+    let pwd; // = "1234";
+    let token = '';
 
     function sayHello() {
         dispatch('message', {
@@ -12,21 +16,36 @@
         });
     }
 
+    async function login(){
+        let user = {email: email, pwd: pwd};
+        const {ok, data} = await httpPost("user/userSrv/auth", user);
+        if (ok) {
+            token = data.token
+        } else {
+            alert('user not found')
+        }
+        // console.log(ok);
+        // console.log(data);
+    }
+
 </script>
+
+
 
 <Card class="mb-3 w-50 mx-auto">
     <CardHeader>
         <CardTitle>Login</CardTitle>
     </CardHeader>
     <CardBody>
-        <Form>
+        <form on:submit|preventDefault={login}>
             <FormGroup>
                 <Label for="userEmail">Email:</Label>
                 <Input
                         type="email"
                         name="userEmail"
                         id="userEmail"
-                        placeholder="Type your email" />
+                        placeholder="Type your email"
+                        bind:value={email}/>
             </FormGroup>
             <FormGroup>
                 <Label for="userPassword">Password:</Label>
@@ -34,10 +53,11 @@
                         type="password"
                         name="userPassword"
                         id="userPassword"
-                        placeholder="Type your password" />
+                        placeholder="Type your password"
+                        bind:value={pwd}/>
             </FormGroup>
-            <!--<Button color="primary" type="submit" >Submit</Button>-->
-            <Button type="submit" on:click={sayHello} >Submit</Button>
-        </Form>
+            <Button type="submit" >Submit</Button>
+        </form>
     </CardBody>
 </Card>
+<!--{email}, {pwd} <br> {token}-->
