dbc364fef2c005d2a69bbc9abb788e5e5f179bea
Juan Peredo <jperedo@bolbeck.com>
2020-07-16 12:14:23 -0500

    added temporary token

--- web/svelte/src/globalUtils/api.js
+++ web/svelte/src/globalUtils/api.js
@@ -1,6 +1,7 @@
 export const apiUrl = "http://localhost:8080/"
 
-let Token = ""
+//TODO: Fetch token from session
+let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyIjp7ImlkIjoyMzQzNzI1MzkxMjkxNjE4MzA1LCJjb21wYW55IjoiRHVjayBJbmMuIn0sImV4cCI6MTU5NDkyODU5NiwiaWF0IjoxNTk0ODQyMTk2LCJpc3MiOiJnb1RlbXAudXNlcnNydiJ9.VhkC0xXndWxcz9B3VPrcXVOCw9FOb8j7AOMgOfqtnuM"
 
 export function httpGet(path) {
     return req(path, 'GET')
@@ -15,14 +16,24 @@ export function httpPut(path,data) {
 }
 
 async function req(path, method, data) {
+    let header = getHeader()
     const res = await fetch(apiUrl + path, {
         method,
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Authorization': 'Bearer ' + Token,
-        },
+        headers: header,
         body: data && JSON.stringify(data)
     })
     let json = await res.json()
+    console.log(res.status)
     return {ok: res.ok, data: json}
+}
+
+function getHeader(){
+    if (Token === "") {
+        return { 'Content-Type': 'application/json'};
+    } else {
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + Token,
+        }
+    }
 }
