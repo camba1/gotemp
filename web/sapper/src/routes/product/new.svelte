392d710b6b3d39cf07541d647cfab91c2838be0f
Juan Peredo <jperedo@bolbeck.com>
2020-08-22 15:57:00 -0500

    initial commit

--- web/sapper/src/routes/product/new.svelte
+++ web/sapper/src/routes/product/new.svelte
@@ -1,1 +1,19 @@
-<h5>New Product!</h5>
+<script>
+
+    import Detail from './_detail.svelte';
+
+    const curDate = new Date()
+    let nextYearDate = new Date()
+    const monthsValid = 12
+    nextYearDate.setMonth( nextYearDate.getMonth() + 1)
+    console.log(nextYearDate)
+    let product = {_key: "", name: "", validityDates: {validFrom: curDate, validThru: nextYearDate}}
+    let slug = ""
+    let extraFields = null
+
+
+</script>
+
+<!--<h5>New Product</h5>-->
+
+<Detail {product} {slug} {extraFields} />
