f36fba2686f73857c8a6a50e41948e56b7bede38
Juan Peredo <jperedo@bolbeck.com>
2020-08-24 17:16:52 -0500

    Moved header and extra fields to components

--- web/sapper/src/routes/product/_detail.svelte
+++ web/sapper/src/routes/product/_detail.svelte
@@ -3,10 +3,15 @@
     import { httpPut } from '../../globalUtils/api'
     import { httpPost } from '../../globalUtils/api'
     import { httpDelete } from '../../globalUtils/api'
+
     import GtDetailCard from './../../components/detailScreen/gtDetailCard.svelte'
     import GtDetailCardFormGrp from './../../components/detailScreen/gtDetailCardFormGrp.svelte'
+    import GTModificationsCard from './../../components/detailScreen/gtModificationsCard.svelte'
+    import GTExtraFieldsCard from './../../components/detailScreen/gtExtraFieldsCard.svelte'
+    import GTDetailHeader from './../../components/detailScreen/gtDetailHeader.svelte'
+
     import Row from 'sveltestrap/src/Row.svelte'
-    import Col from 'sveltestrap/src/Col.svelte'
+    import Container from 'sveltestrap/src/Container.svelte'
     import Button from 'sveltestrap/src/Button.svelte'
     import Input from 'sveltestrap/src/Input.svelte'
 
@@ -14,35 +19,41 @@
     export let slug;
     export let extraFields;
 
-    let tmpDateFrom = new Date(product.validityDates.validFrom).toLocaleDateString();
-    let tmpDateThru = new Date(product.validityDates.validThru).toLocaleDateString();
+    let tmpDateFrom = new Date(product.validityDates.validFrom).toLocaleDateString("en-CA",{year:"numeric",month:"2-digit", day:"2-digit"});
+    let tmpDateThru = new Date(product.validityDates.validThru).toLocaleDateString("en-CA",{year:"numeric",month:"2-digit", day:"2-digit"});
 
     let addresses = {update: "product/productSrv/UpdateProduct",
         create: "product/productSrv/CreateProduct",
         delete: "product/productSrv/DeleteProduct",
+        reload: "product/",
         previousPage: "/product"}
+
+    let inProgress = false
 
 
     async function  handleSave() {
-        alert('Saving...');
-        console.log(product)
+
+        inProgress = true
+console.log(product.validityDates.validFrom)
         const {ok, data} = await (slug
                                     ? httpPut(addresses.update, product)
                                     : httpPost(addresses.create, product))
-        console.log(ok)
         if (ok) {
             if (isObjectEmpty(data)) {
                 alert('Product not saved')
+            } else if (!slug) {
+                goto(addresses.reload + data.product._key)
             }
         } else {
             alert('Product not saved')
         }
 
+        inProgress = false
     }
 
     async function handleDelete() {
-        console.log(slug)
-        console.log(product)
+        inProgress = true
+
         const paramString = new URLSearchParams({ _key: `"${slug}"` })
         const {ok, data} = await httpDelete(`${addresses.delete}?${paramString.toString()}`);
         console.log(data)
@@ -52,10 +63,11 @@
             alert('Product not deleted')
         }
 
+        inProgress = false
     }
 
     async function backToSearch() {
-        // alert('back to base')
+
         await goto(addresses.previousPage)
     }
 
@@ -64,58 +76,84 @@
         return true;
     }
 
+    function updateVF(event) {
+        updateVD("validFrom", event.target.value)
+    }
+
+    function updateVT(event) {
+        updateVD("validThru", event.target.value)
+    }
+
+    function updateVD(dateToUpdate, newDateString){
+        let foundVD = false
+        if (product) {
+            if (product.validityDates) {
+                let parts = newDateString.split('-');
+                let VD = new Date(parts[0], parts[1] - 1, parts[2]);
+                // let VD = new Date(event.target.value)
+                if (isValidDate(VD)){
+                    product.validityDates[dateToUpdate] = VD
+                }
+                foundVD = true
+            }
+        }
+        if (!foundVD) {
+            alert('Unable to populate validity date')
+        }
+    }
+
+    function isValidDate(d) {
+        return d instanceof Date && !isNaN(d);
+    }
+
 </script>
 
-{#if product}
+<Container>
 
-    <Row>
-        <Col class="col-8">
-            <h4>Product: {product.name}</h4>
-        </Col>
-        <Col class="text-right" >
-            <Button size="sm" on:click={handleSave}><span><i class="fas fa-save"></i> Save</span></Button>
-            {#if slug}
-                <Button size="sm" on:click={handleDelete}><span><i class="fas fa-trash-alt"></i> Delete</span></Button>
-            {/if}
-            <Button size="sm" on:click="{backToSearch}"><span><i class="fas fa-arrow-alt-circle-left"></i> Back</span></Button>
+    {#if product}
 
-        </Col>
-    </Row>
-
-    <Row>
-        <GtDetailCard cardHeader="Information">
-            <GtDetailCardFormGrp lblFor="id" lblText="Id:">
-                <Input id="id" class="form-control form-control-sm" name="id" type="text" readonly bind:value={product._key}/>
-            </GtDetailCardFormGrp>
-            <GtDetailCardFormGrp lblFor="name" lblText="Name:">
-                <Input id="name"class="form-control form-control-sm"  name="name" type="text" readonly={false} bind:value={product.name}/>
-            </GtDetailCardFormGrp>
-            <GtDetailCardFormGrp lblFor="hierLevel" lblText="Level:">
-                <Input id="hierLevel"class="form-control form-control-sm"  name="hierLevel" type="text" readonly={false} bind:value={product.hierarchyLevel}/>
-            </GtDetailCardFormGrp>
-        </GtDetailCard>
-
-        <GtDetailCard cardHeader="Validity">
-            <GtDetailCardFormGrp lblFor="validFrom" lblText="Valid From:">
-                <Input id="validFrom" class="form-control form-control-sm" name="validFrom" type="date" readonly={false}  bind:value={tmpDateFrom}/>
-            </GtDetailCardFormGrp>
-            <GtDetailCardFormGrp lblFor="validThru" lblText="Valid Thru:">
-                <Input id="validThru"class="form-control form-control-sm"  name="validThru" type="date" readonly={false}  bind:value={tmpDateThru}/>
-            </GtDetailCardFormGrp>
-        </GtDetailCard>
-    </Row>
-    {#if extraFields}
         <Row>
-            <GtDetailCard cardHeader="Other Fields">
-                {#each extraFields as field }
-                    <GtDetailCardFormGrp lblFor="test2" lblText="{field.name}:">
-                        <Input id="test2" class="form-control form-control-sm" name="test2" plaintext readonly={false}  bind:value={field.value}/>
-                    </GtDetailCardFormGrp>
-                {/each}
+            <GTDetailHeader label="Product" inProgress={inProgress} name={product.name} slug={slug}
+                            on:handleSave={handleSave}
+                            on:handleDelete={handleDelete}
+                            on:backToSearch={backToSearch} />
+        </Row>
+
+        <Row>
+            <GtDetailCard cardHeader="Information">
+                <GtDetailCardFormGrp lblFor="id" lblText="Id:">
+                    <Input id="id" class="form-control form-control-sm" name="id" type="text" readonly bind:value={product._key}/>
+                </GtDetailCardFormGrp>
+                <GtDetailCardFormGrp lblFor="name" lblText="Name:">
+                    <Input id="name"class="form-control form-control-sm"  name="name" type="text" readonly={false} bind:value={product.name}/>
+                </GtDetailCardFormGrp>
+                <GtDetailCardFormGrp lblFor="hierLevel" lblText="Level:">
+                    <Input id="hierLevel"class="form-control form-control-sm"  name="hierLevel" type="text" readonly={false} bind:value={product.hierarchyLevel}/>
+                </GtDetailCardFormGrp>
+            </GtDetailCard>
+
+            <GtDetailCard cardHeader="Validity">
+                <GtDetailCardFormGrp lblFor="validFrom" lblText="Valid From:">
+                    <Input id="validFrom" class="form-control form-control-sm" name="validFrom" type="date" placeholder="yyyy-mm-dd" readonly={false} on:input={updateVF} bind:value={tmpDateFrom}/>
+                </GtDetailCardFormGrp>
+                <GtDetailCardFormGrp lblFor="validThru" lblText="Valid Thru:">
+                    <Input id="validThru"class="form-control form-control-sm"  name="validThru" type="date" placeholder="yyyy-mm-dd" readonly={false} on:input={updateVT} bind:value={tmpDateThru}/>
+                </GtDetailCardFormGrp>
             </GtDetailCard>
         </Row>
+
+        <Row>
+            {#if product.modifications}
+                <GTModificationsCard createDateTime={product.modifications.createDate} updateDateTime={product.modifications.updateDate} modifiedBy={product.modifications.modifiedBy} />
+            {/if}
+            {#if extraFields}
+                <GTExtraFieldsCard extraFields={extraFields} />
+            {/if}
+        </Row>
+
+    {:else}
+        <h3>No data found for product</h3>
+        <Button size="sm" on:click="{backToSearch}"><span><i class="fas fa-arrow-alt-circle-left"></i> Back</span></Button>
     {/if}
-{:else}
-    <h3>No data found for product</h3>
-    <Button size="sm" on:click="{backToSearch}"><span><i class="fas fa-arrow-alt-circle-left"></i> Back</span></Button>
-{/if}
+
+</Container>
