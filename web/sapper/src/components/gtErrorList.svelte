1030a159090ce193bfc29361be19627657a26ade
Juan Peredo <jperedo@bolbeck.com>
2020-08-25 14:45:31 -0500

    initial commit

---
+++ web/sapper/src/components/gtErrorList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Row from 'sveltestrap/src/Row.svelte'
+    import Col from 'sveltestrap/src/Col.svelte'
+
+    export let errorList;
+
+</script>
+
+<style>
+    pre {
+        display: block;
+        font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        font-weight: bold;
+        white-space: pre;
+        margin: 1em 0;
+        color: #8B0000;
+    }
+</style>
+{#if errorList}
+    <Row>
+        <Col class="text-center" >
+            <pre>{errorList.detail}</pre>
+        </Col>
+    </Row>
+{/if}
