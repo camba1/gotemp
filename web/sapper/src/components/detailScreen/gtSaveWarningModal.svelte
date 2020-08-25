ae6f939cef005e8098fe7ac762b1cc5c95fa536a
Juan Peredo <jperedo@bolbeck.com>
2020-08-25 17:03:25 -0500

    initial commit

---
+++ web/sapper/src/components/detailScreen/gtSaveWarningModal.svelte
@@ -0,0 +1,35 @@
+<script>
+
+    import Button from 'sveltestrap/src/Button.svelte'
+    import Modal from 'sveltestrap/src/Modal.svelte'
+    import ModalBody from 'sveltestrap/src/ModalBody.svelte'
+    import ModalFooter from 'sveltestrap/src/ModalFooter.svelte'
+    import ModalHeader from 'sveltestrap/src/ModalHeader.svelte'
+
+    export let open = false
+    export let warningText
+    const toggle = () => (open = !open)
+
+</script>
+
+<style>
+    pre {
+        display: block;
+        font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        white-space: pre;
+        margin: 1em 0;
+    }
+</style>
+
+<div>
+    <Modal isOpen={open} {toggle}>
+        <ModalHeader class="bg-light" {toggle}><span style="color: darkgoldenrod"><i class="fas fa-exclamation-triangle"></i> Warning</span></ModalHeader>
+        <ModalBody>
+            <span>Data was saved with warnings:</span>
+            <pre>{warningText}</pre>
+        </ModalBody>
+        <ModalFooter class="bg-light">
+            <Button on:click={toggle}>OK</Button>
+        </ModalFooter>
+    </Modal>
+</div>
