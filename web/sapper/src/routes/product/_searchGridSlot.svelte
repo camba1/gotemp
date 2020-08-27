089b6798bc7ea301ef14c658abf5bb156eecf870
Juan Peredo <jperedo@bolbeck.com>
2020-08-27 15:52:44 -0500

    initial commit

---
+++ web/sapper/src/routes/product/_searchGridSlot.svelte
@@ -0,0 +1,22 @@
+<script>
+    /**
+     * Component to be displayed within the search grid
+     */
+
+    /**
+     * Data to be displayed
+     * @type {object}
+     */
+    export let tblData;
+</script>
+
+{#each tblData as dataRow}
+    <tr>
+        <th scope="row"><a href="customer/{dataRow._key}"> {dataRow._key} </a></th>
+        <td>{dataRow.name}</td>
+        <td>{new Date(dataRow.validityDates.validFrom).toLocaleDateString("en-CA",{year:"numeric",month:"2-digit", day:"2-digit"})}</td>
+        <td>{new Date(dataRow.validityDates.validThru).toLocaleDateString("en-CA",{year:"numeric",month:"2-digit", day:"2-digit"})}</td>
+        <td>{dataRow.hierarchyLevel}</td>
+<!--        <td>{dataRow.extraFields}</td>-->
+    </tr>
+{/each}
