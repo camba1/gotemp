eb9ebf60c04d9e64ef1b6002d4684c6d1129f4e2
Juan Peredo <jperedo@bolbeck.com>
2020-08-27 15:53:21 -0500

    Added customer addresses and added getall to product's

--- web/sapper/src/globalUtils/addresses.js
+++ web/sapper/src/globalUtils/addresses.js
@@ -1,7 +1,19 @@
 export let productAddresses = { get: "product/productSrv/GetProductById",
+    getAll: "product/productSrv/GetProducts",
     update: "product/productSrv/UpdateProduct",
     create: "product/productSrv/CreateProduct",
     delete: "product/productSrv/DeleteProduct",
     new: "/product/new",
     reload: "product/",
-    previousPage: "/product"}
+    previousPage: "/product",
+    searchPreviousPage: "/"}
+
+export let customerAddresses = { get: "customer/customerSrv/GetCustomerById",
+    getAll: "customer/customerSrv/GetCustomers",
+    update: "customer/customerSrv/UpdateCustomer",
+    create: "customer/customerSrv/CreateCustomer",
+    delete: "customer/customerSrv/DeleteCustomer",
+    new: "/customer/new",
+    reload: "customer/",
+    previousPage: "/customer",
+    searchPreviousPage: "/"}
