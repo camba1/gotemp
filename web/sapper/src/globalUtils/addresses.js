de29d07ddd65d3ee101e275f95adbb0ce2ed69f6
Juan Peredo <jperedo@bolbeck.com>
2020-08-27 12:11:22 -0500

    initial commit

---
+++ web/sapper/src/globalUtils/addresses.js
@@ -0,0 +1,7 @@
+export let productAddresses = { get: "product/productSrv/GetProductById",
+    update: "product/productSrv/UpdateProduct",
+    create: "product/productSrv/CreateProduct",
+    delete: "product/productSrv/DeleteProduct",
+    new: "/product/new",
+    reload: "product/",
+    previousPage: "/product"}
