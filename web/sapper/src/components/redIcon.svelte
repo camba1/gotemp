f32cc7196c50ac82b8dd08116aa40967a2b00764
Juan Peredo <jperedo@bolbeck.com>
2020-10-27 19:07:01 -0500

    initial commit

---
+++ web/sapper/src/components/redIcon.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let iconName
+    let color = "rgb(204,0,0)"
+</script>
+
+<style>
+    .lnr {
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.05em;
+        stroke-width: 1;
+    }
+
+    .services-icon {
+        margin-bottom: 20px;
+        font-size: 30px;
+    }
+</style>
+
+<span style="color: {color};">
+    <i class="fas {iconName} lnr services-icon"></i>
+</span>
