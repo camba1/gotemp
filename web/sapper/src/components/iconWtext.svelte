8b9504f1a0cef401d80a92740af0e7c9c68ab31c
Juan Peredo <jperedo@bolbeck.com>
2020-10-27 19:56:04 -0500

    initial commit

---
+++ web/sapper/src/components/iconWtext.svelte
@@ -0,0 +1,13 @@
+<script>
+    import RedIcon from '../components/redIcon.svelte';
+
+    export let data
+</script>
+
+<div class="col-sm-6 col-lg-4 mb-3">
+    <RedIcon  iconName="{data.iconName}" />
+    <h5>{data.header}</h5>
+    {#each data.techList as tech}
+        <p class="text-muted">{tech}</p>
+    {/each}
+</div>
