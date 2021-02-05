bd379c9741b539df785d5cf287fbeeac29071ede
Juan Peredo <jperedo@bolbeck.com>
2021-02-05 09:32:36 -0600

    initial commit

---
+++ vault/scripts/deleteAllSrv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Delete all services artifacts
+./file/scripts/deleteSrv.sh auditsrv
+./file/scripts/deleteSrv.sh customersrv
+./file/scripts/deleteSrv.sh productsrv
+./file/scripts/deleteSrv.sh promotionsrv
+./file/scripts/deleteSrv.sh usersrv
---
+++ vault/scripts/deleteSrv.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Check if we got a vault token to be able to login
+if [ "$#" -ne 1 ]
+  then
+    echo "No service name supplied" >&2
+    exit 1
+fi
+
+# Delete role
+vault delete auth/kubernetes/role/gotemp-$1
+# Delete policy
+vault policy delete gotemp-$1
+
+#Delete secrets (all versions and metadata)
+vault kv metadata delete gotempkv/database/postgresql/$1
+vault kv metadata delete gotempkv/database/redis/$1
+vault kv metadata delete gotempkv/database/arangodb/$1
+vault kv metadata delete gotempkv/broker/nats/$1
