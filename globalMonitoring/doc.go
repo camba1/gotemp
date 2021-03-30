3bc2e3b5ceb316c69da744c5d5e0b025a0750a92
Juan Peredo <jperedo@bolbeck.com>
2021-03-30 17:32:25 -0500

    initial commit

---
+++ globalMonitoring/doc.go
@@ -0,0 +1,7 @@
+/*
+Package globalMonitoring contains observability utilities that are common to all services.
+It is composed of the following files:
+- prometheus: Enables scraping of metrics to prometheus
+*/
+
+package globalMonitoring
