c5d170ef9d4c7ec7290de9b84f0cfac9b9b67b05
Juan Peredo <jperedo@bolbeck.com>
2021-03-31 10:27:54 -0500

    initial commit

---
+++ product/server/monitoring.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/micro/go-micro/v2/server"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"goTemp/globalMonitoring"
+	"net/http"
+)
+
+const (
+	// serviceId numeric service identifier
+	serviceId = "3"
+	// serviceMetricsPrefix prefix for all metrics related to this service
+	serviceMetricsPrefix = "goTemp_"
+	// metricsEndPoint is the address where we can scrape metrics
+	metricsEndPoint = "/metrics"
+	//httpPort is the port where the http server for metrics is listening
+	httpPort = ":2112"
+)
+
+// runHttp runs a secondary server to handle metrics scraping
+func runHttp() {
+	http.Handle(metricsEndPoint, promhttp.Handler())
+	http.ListenAndServe(httpPort, nil)
+}
+
+//newMetricsWrapper Create a new metrics wrapper to configure the data to be scraped for monitoring
+func newMetricsWrapper() server.HandlerWrapper {
+	// TODO: get version number from external source
+	return globalMonitoring.NewMetricsWrapper(
+		globalMonitoring.ServiceName(serviceName),
+		globalMonitoring.ServiceID(serviceId),
+		globalMonitoring.ServiceVersion("latest"),
+		globalMonitoring.ServiceMetricsPrefix(serviceMetricsPrefix),
+		globalMonitoring.ServiceMetricsLabelPrefix(serviceMetricsPrefix),
+	)
+}
